9a1d564e7cb8e602ca275bca4a439181c7188a32
funkyfelix <felix.koschi@gmail.com>
2018-09-27 13:25:38 +0200

    Basic SetUp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UiModule } from './ui/ui.module';
+import { LayoutComponent } from './ui/layout/layout.component';
+
+const routes: Routes = [
+  { path: '', component: LayoutComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -15,8 +23,10 @@ import { environment } from '../environments/environment';
     BrowserModule,
     HttpClientModule,
     AngularFireModule.initializeApp(environment.firebase),
+    UiModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
